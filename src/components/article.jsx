f02ceb3a66c1f45c2689ce5a6ca73b0bb4823f4e
Mathew <mathewkariuki2005@gmail.com>
2025-07-19 17:25:34 +0300

    modified the file

---
+++ src/components/article.jsx
@@ -0,0 +1,21 @@
+function Article({title,date="January 1, 1970",preview,minutes}){
+
+    let emoji="";
+    if(minutes<30){
+        const cups=Math.ceil(minutes/5);
+        emoji = "☕️".repeat(cups);   
+    }else{
+        const boxes=Math.ceil(minutes/10);
+        emoji = "🍱".repeat(boxes);
+
+    }
+
+    return(
+        <article>
+    <h3>{title}</h3>
+    <small>{date} {emoji} {minutes}</small>
+    <p>{preview}</p>
+        </article>
+    )
+}
+export default Article;
